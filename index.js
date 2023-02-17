b856f90cc26c7e0f512305337540a31462da129e
Samuel Motas <smlmotas@gmail.com>
2023-02-17 11:00:57 -0500

    body-parser

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { app, express } = require("./server")
 const port = 3000
 const path = require("path")
+const bodyParser = require("body-parser")
 
 // Connection to database
 require("./mongo")
+
 
 // Controllers
 const { createUser, logUser } = require("./controllers/users")
@@ -12,6 +14,7 @@ const { getSauces, createSauces, getSauceById, deleteSauce, modifySauces, likeSa
 // Middleware
 const { upload } = require("./middleware/multer")
 const { authenticateUser } = require("./middleware/auth")
+app.use(bodyParser.json())
 
 
 // Routes
